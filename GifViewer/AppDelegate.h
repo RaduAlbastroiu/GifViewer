a731dc682cb08f39b17f85532148cd12684c8fc2
RaduAlbastroiu <albastroiuradug@gmail.com>
2018-08-16 12:13:53 +0300

    Initial Commit

---
+++ GifViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GifViewer
+//
+//  Created by Radu Albastroiu on 16/08/2018.
+//  Copyright © 2018 Radu Albastroiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GifViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GifViewer
+//
+//  Created by Radu Albastroiu on 16/08/2018.
+//  Copyright © 2018 Radu Albastroiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
